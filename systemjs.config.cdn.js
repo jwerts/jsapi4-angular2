52c15fe93400e2f6d8edbbfbf21518ac36b6e9af
Josh Werts <joshwerts@gmail.com>
2016-06-16 09:08:30 -0400

    update system config for rc2

--- systemjs.config.cdn.js
+++ systemjs.config.cdn.js
@@ -1,28 +1,25 @@
 /**
- * PLUNKER VERSION (based on systemjs.config.js in angular.io)
- * System configuration for Angular 2 samples
+ * System configuration for Angular 2 apps
  * Adjust as necessary for your application needs.
  */
 (function(global) {
 
-  var ngVer = '@2.0.0-rc.1'; // lock in the angular package version; do not let it float to current!
+  var ngVer = '@2.0.0-rc.2'; // lock in the angular package version; do not let it float to current!
 
-  //map tells the System loader where to look for things
-  var  map = {
-    'app':                        'app',
+  // map tells the System loader where to look for things
+  var map = {
+    'app':                        'app', // 'dist',
 
     '@angular':                   'https://npmcdn.com/@angular', // sufficient if we didn't pin the version
     'angular2-in-memory-web-api': 'https://npmcdn.com/angular2-in-memory-web-api', // get latest
     'rxjs':                       'https://npmcdn.com/rxjs@5.0.0-beta.6',
-    'ts':                         'https://npmcdn.com/plugin-typescript@4.0.10/lib/plugin.js',
-    'typescript':                 'https://npmcdn.com/typescript@1.8.10/lib/typescript.js',
- };
+  };
 
-  //packages tells the System loader how to load when no filename and/or no extension
+  // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
-    'app':                        { main: 'main.ts',  defaultExtension: 'ts' },
+    'app':                        { main: 'main.js',  defaultExtension: 'js' },
     'rxjs':                       { defaultExtension: 'js' },
-    'angular2-in-memory-web-api': { defaultExtension: 'js' },
+    'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
   };
 
   var ngPackageNames = [
@@ -43,34 +40,99 @@
     map['@angular/'+pkgName] = 'https://npmcdn.com/@angular/' + pkgName + ngVer;
   });
 
+  // Bundled (~40 requests):
+  function packUmd(pkgName) {
+    packages['@angular/'+pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
+  }
+
+  var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+
   // Add package entries for angular packages
-  ngPackageNames.forEach(function(pkgName) {
-
-    // Bundled (~40 requests):
-    packages['@angular/'+pkgName] = { main: pkgName + '.umd.js', defaultExtension: 'js' };
-
-    // Individual files (~300 requests):
-    //packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
-  });
+  ngPackageNames.forEach(setPackageConfig);
 
   var config = {
-    // DEMO ONLY! REAL CODE SHOULD NOT TRANSPILE IN THE BROWSER
-    transpiler: 'ts',
-    typescriptOptions: {
-      tsconfig: true
-    },
-    meta: {
-      'typescript': {
-        "exports": "ts"
-      }
-    },
     map: map,
     packages: packages
-  }
+  };
 
   System.config(config);
 
 })(this);
+
+
+/**
+ * PLUNKER VERSION (based on systemjs.config.js in angular.io)
+ * System configuration for Angular 2 samples
+ * Adjust as necessary for your application needs.
+ */
+// (function(global) {
+
+//   var ngVer = '@2.0.0-rc.2'; // lock in the angular package version; do not let it float to current!
+
+//   //map tells the System loader where to look for things
+//   var  map = {
+//     'app':                        'app',
+
+//     '@angular':                   'https://npmcdn.com/@angular', // sufficient if we didn't pin the version
+//     'angular2-in-memory-web-api': 'https://npmcdn.com/angular2-in-memory-web-api', // get latest
+//     'rxjs':                       'https://npmcdn.com/rxjs@5.0.0-beta.6',
+//     'ts':                         'https://npmcdn.com/plugin-typescript@4.0.10/lib/plugin.js',
+//     'typescript':                 'https://npmcdn.com/typescript@1.8.10/lib/typescript.js',
+//  };
+
+//   //packages tells the System loader how to load when no filename and/or no extension
+//   var packages = {
+//     'app':                        { main: 'main.ts',  defaultExtension: 'ts' },
+//     'rxjs':                       { defaultExtension: 'js' },
+//     'angular2-in-memory-web-api': { defaultExtension: 'js' },
+//   };
+
+//   var ngPackageNames = [
+//     'common',
+//     'compiler',
+//     'core',
+//     'http',
+//     'platform-browser',
+//     'platform-browser-dynamic',
+//     'router',
+//     'router-deprecated',
+//     'upgrade',
+//   ];
+
+//   // Add map entries for each angular package
+//   // only because we're pinning the version with `ngVer`.
+//   ngPackageNames.forEach(function(pkgName) {
+//     map['@angular/'+pkgName] = 'https://npmcdn.com/@angular/' + pkgName + ngVer;
+//   });
+
+//   // Add package entries for angular packages
+//   ngPackageNames.forEach(function(pkgName) {
+
+//     // Bundled (~40 requests):
+//     packages['@angular/'+pkgName] = { main: pkgName + '.umd.js', defaultExtension: 'js' };
+
+//     // Individual files (~300 requests):
+//     //packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
+//   });
+
+//   var config = {
+//     // DEMO ONLY! REAL CODE SHOULD NOT TRANSPILE IN THE BROWSER
+//     transpiler: 'ts',
+//     typescriptOptions: {
+//       tsconfig: true
+//     },
+//     meta: {
+//       'typescript': {
+//         "exports": "ts"
+//       }
+//     },
+//     map: map,
+//     packages: packages
+//   }
+
+//   System.config(config);
+
+// })(this);
 
 
 /*
