cea57531a1d32a8b7ce565ae171171911dde37c8
Josh Werts <joshwerts@gmail.com>
2016-06-23 15:19:18 -0400

    cleanup and better typings handling

--- typings/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="globals/angular-protractor/index.d.ts" />
-/// <reference path="globals/arcgis-js-api/index.d.ts" />
-/// <reference path="globals/core-js/index.d.ts" />
-/// <reference path="globals/dojo/index.d.ts" />
-/// <reference path="globals/jasmine/index.d.ts" />
-/// <reference path="globals/selenium-webdriver/index.d.ts" />
