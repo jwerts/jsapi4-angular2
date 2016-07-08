d7fca972f27f78af5f7e5d1629920102a0f0770c
Josh Werts <joshwerts@gmail.com>
2016-07-07 20:39:05 -0400

    revert config

--- systemjs.config.cdn.js
+++ systemjs.config.cdn.js
@@ -12,8 +12,8 @@
 
     '@angular':                   'https://npmcdn.com/@angular', // sufficient if we didn't pin the version
     'angular2-in-memory-web-api': 'https://npmcdn.com/angular2-in-memory-web-api', // get latest
-    'rxjs':                       'https://npmcdn.com/rxjs@5.0.0-beta.6',
-    'esri':                       'http://js.arcgis.com/4.0/'
+    'rxjs':                       'https://npmcdn.com/rxjs@5.0.0-beta.6'
+    // 'esri':                       'http://js.arcgis.com/4.0/'
   };
 
   // packages tells the System loader how to load when no filename and/or no extension
